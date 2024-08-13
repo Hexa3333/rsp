b9de8bf9555b9889fe7fe1dac3e42ecd2e94fd39
Alp Yılmaz <Hexa3333@protonmail.com>
2024-08-14 02:36:32 +0300

    Did a lot of things

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,40 @@
  * There is no trimming and stuff like that +
  * There is no way of checking errors in code; write proper rsp files */
 
+/* TODO: Change line-per to multi line */
+
+/* TODO: special headers should be documented
+ * _DEFAULT_: default header that is empty ([]) */
+
+/* TODO: ADD TRIM FUNCTIONS */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 enum TOKEN
 {
+  TOKEN_POUND = '#',
   TOKEN_LBRACK = '[',
   TOKEN_RBRACK = ']',
-  TOKEN_QUOTE = '\"',
+  TOKEN_QUOTE = '\"'
 };
+
+enum struct SPECIAL_HEADER
+{};
 
 struct rsp_data
 {
   int headerCount;
   char** headers;
-
-  int* responseCounts;
-  int** responseIndexes;
+  int* headerStartIndexes;
+  int* headerReplyCount;
 };
 
 struct rsp
 {
-  char* userName;
+  char userName[32];
   rsp_data data;
 };
 
@@ -35,11 +46,14 @@ char** GetResponsesFromHeader(rsp_data in, const char* header)
 }
 
 /* TODO: Shared headers:
- * [sa, selaminaleykum] */
+ * [hi, hello] */
 rsp CreateRspFromFile(const char* username)
 {
   rsp ret = {};
+  strcpy(ret.userName, username);
   ret.data.headers = (char**)malloc(sizeof(char**));
+  ret.data.headerStartIndexes = (int*)malloc(sizeof(int*));
+  ret.data.headerReplyCount = (int*)malloc(sizeof(int*));
 
   /* Add trailing .rsp */
   char fileName[128] = {};
@@ -57,7 +71,7 @@ rsp CreateRspFromFile(const char* username)
   char c;
   while((c = fgetc(fp)) != EOF)
   {
-    if (c == '#') /* Comment */
+    if (c == TOKEN_POUND) /* Comment */
     {
       char _trash[256];
       fgets(_trash, 256, fp);
@@ -73,22 +87,29 @@ rsp CreateRspFromFile(const char* username)
         header[i] = c;
         ++i;
       }
-      /* TODO: special headers should be documented */
       /* if empty == _DEFAULT_ */
       if (header[0] == 0)
         ret.data.headers[ret.data.headerCount] = strdup("_DEFAULT_");
       else
         ret.data.headers[ret.data.headerCount] = strdup(header);
 
-      char _trash [32];
-      fgets(_trash, 32, fp);
 
+      ret.data.headerStartIndexes[ret.data.headerCount] = ftell(fp);
+      ret.data.headerReplyCount[ret.data.headerCount] = 0;
       ++ret.data.headerCount;
     }
-  }
-  fseek(fp, 0, SEEK_SET);
 
-  /* Indexing algorithm */
+    /* ++ header reply count */
+    if (c == TOKEN_QUOTE)
+    {
+      /* TODO: there must be a better way: */
+      /* -1 because the block before this modifies the headerCount */
+      ++ret.data.headerReplyCount[ret.data.headerCount-1];
+      /* clear line */
+      char _trash[512];
+      fgets(_trash, 512, fp);
+    }
+  }
 
   fclose(fp);
   return ret;
@@ -99,18 +120,70 @@ void FreeRsp(rsp in)
   for (int i = 0; i < in.data.headerCount; i++)
   { free(in.data.headers[i]); }
   free(in.data.headers);
+  free(in.data.headerStartIndexes);
+  free(in.data.headerReplyCount);
 }
 
-/* TODO: ADD TRIM FUNCTIONS */
-/* TODO: Parse/Debug the file better */
+char* GetReply(rsp in, const char* header)
+{
+  char ret[512] = {};
+
+  /* Add trailing .rsp */
+  char fileName[128] = {};
+  strcpy(fileName, in.userName);
+  strcpy(&fileName[strlen(in.userName)], ".rsp");
+
+  /* Find the header index */
+  int headi = 0;
+  for (headi = 0; headi < in.data.headerCount; ++headi)
+  {
+    /* found one */
+    if (strcmp(in.data.headers[headi], header) == 0)
+    {
+      break;
+    }
+  }
+
+  /* TODO: Maybe a better random gen ? - Might not be necessary though */
+
+  FILE* fp = fopen(fileName, "r");
+  fseek(fp, in.data.headerStartIndexes[headi], SEEK_SET);
+
+  srand(time(0));
+  unsigned int callIndex = rand() % in.data.headerReplyCount[headi];
+  char buffer[512] = {};
+  while (fgets(buffer, 512, fp) != NULL)
+  {
+    if (buffer[0] == TOKEN_POUND ||
+        buffer[0] == '\n') continue;
+    
+    /* if the quote has no enclosing counterpart... */
+    if (callIndex == 0 &&
+        buffer[0] == TOKEN_QUOTE)
+    {
+      /* trim and save */
+      strncpy(ret, buffer+1, strlen(buffer)-3);
+    }
+    
+    --callIndex;
+  }
+
+  fclose(fp);
+  return strdup(ret);
+}
 
 int main(void)
 {
   rsp hexa = CreateRspFromFile("../rsp/hexa3333");
+  /*
   for (int i = 0; i < hexa.data.headerCount; i++)
   {
     printf("%s\n", hexa.data.headers[i]);
-  }
+  } */
+
+  char* reply = GetReply(hexa, "allah yok");
+  printf("%s\n", reply);
+  free(reply);
 
   FreeRsp(hexa);
   return 0;
